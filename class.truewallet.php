4989b4b7381b1ed8f58f5c2142a2ad1e33f92093
exzajung <ex_zajung@live.com>
2017-07-27 15:42:46 +0700

    Update
    Update getTran , CheckTran ทำรายการผ่าน TureWallet

--- class.truewallet.php
+++ class.truewallet.php
@@ -8,12 +8,14 @@ class TrueWallet{
 	private $api_signin = "https://api-ewm.truemoney.com/api/v1/signin?&"; 
 	private $api_profile = "https://api-ewm.truemoney.com/api/v1/profile/"; 
 	private $api_topup = "https://api-ewm.truemoney.com/api/api/v1/topup/mobile/"; 
+	private $api_gettran = "https://api-ewm.truemoney.com/api/v1/profile/transactions/history/"; 
+	private $api_checktran = "https://api-ewm.truemoney.com/api/v1/profile/activities/"; 
 	private $device_os = "android"; 
 	private $device_id = "d520d0d12d0d48cb89394905168c6ed5"; 
 	private $device_type = "CPH1611"; 
 	private $device_version = "6.0.1"; 
 	private $app_name = "wallet"; 
-	private $app_version = "2.9.14"; 
+	private $app_version = "4.0.1"; 
 	private $deviceToken = "fUUbZJ9nwBk:APA91bHHgBBHhP9rqBEon_BtUNz3rLHQ-sYXnezA10PRSWQTwFpMvC9QiFzh-CqPsbWEd6x409ATC5RVsHAfk_-14cSqVdGzhn8iX2K_DiNHvpYfMMIzvFx_YWpYj5OaEzMyIPh3mgtx"; 
 	private $mobileTracking = "dJyFzn\/GIq7lrjv2RCsZbphpp0L\/W2+PsOTtOpg352mgWrt4XAEAAA=="; 
 	//End Config
@@ -56,6 +58,19 @@ class TrueWallet{
 		return $this->wallet_curl($url,true,$header);
 	}
 	
+	public function getTran($token,$start,$end){
+		$url = $this->api_gettran.$token.'/?startDate='.$start.'&endDate='.$end.'&limit=20&page=1&type=&action=';
+		$header = array("Host: api-ewm.truemoney.com");
+		return $this->wallet_curl($url,false,$header);	
+		//return $url;	
+	}
+	
+	public function CheckTran($token,$id){
+		$url = $this->api_checktran.$id.'/detail/'.$token.'?&device_os=android&device_id='.$this->device_id.'&device_type='.$this->device_type.'&device_version='.$this->device_version.'&app_name='.$this->app_name.'&app_version='.$this->app_version;
+		$header = array("Host: api-ewm.truemoney.com");
+		return $this->wallet_curl($url,false,$header);	
+	}
+	
 	private function wallet_curl($url,$data,$header){	
 		$ch = curl_init();
 		curl_setopt($ch,CURLOPT_URL,$url);
@@ -67,7 +82,7 @@ class TrueWallet{
 			curl_setopt($ch,CURLOPT_POSTFIELDS, $data);         
 		}                                  
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);   
-		curl_setopt($ch,CURLOPT_USERAGENT,'');
+		curl_setopt($ch,CURLOPT_USERAGENT,'okhttp/3.8.0');
 		$result = curl_exec($ch);
 		return $result;
 	}
